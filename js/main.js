a15614b3f1518391ce74affe7f1a7bec6734b835
topaz-rep <kenguru87@gmail.com>
2020-02-12 13:12:15 +0200

    added some func

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const display = document.querySelector('.display');
-let resRec = 0; 
+let resRec = 0;
 let opsBtn = 0;
 
 document.querySelectorAll('.digits button')
-    .forEach( button => button.addEventListener('click', digitPressed));
+    .forEach(button => button.addEventListener('click', digitPressed));
 
 function digitPressed(ev) {
     if (resRec === 1 && opsBtn === 0) clearResults();
@@ -11,24 +11,25 @@ function digitPressed(ev) {
 }
 
 document.querySelectorAll('.ops button')
-    .forEach( button => button.addEventListener('click', opsPressed));
-    
+    .forEach(button => button.addEventListener('click', opsPressed));
+
 function opsPressed(ev) {
-        if (display.value.slice(-1) == '+' || display.value.slice(-1) == '*' || display.value.slice(-1) == '/' || display.value.slice(-1) == '-') {
-            display.value = display.value.substring(0, display.value.length - 1) + ev.target.innerText;
-        } else {
-            display.value += ev.target.innerText;
-        }
-        opsBtn = 1;
+    if (display.value.slice(-1) == '+' || display.value.slice(-1) == '*' || display.value.slice(-1) == '/' || display.value.slice(-1) == '-') {
+        display.value = display.value.substring(0, display.value.length - 1) + ev.target.innerText;
+    } else {
+        display.value += ev.target.innerText;
+    }
+    opsBtn = 1;
 }
 
 document.querySelector('.eq').addEventListener('click', calculate);
 
 function calculate() {
+    if (display.value.slice(-1) == '+' || display.value.slice(-1) == '*' || display.value.slice(-1) == '/' || display.value.slice(-1) == '-') display.value = display.value.substring(0, display.value.length - 1);
     (eval(display.value) == Infinity || eval(display.value) == -Infinity) ? display.value = 'You can\'t divide by zero!'
-      : display.value = eval(display.value);
-      resRec = 1;
-      opsBtn = 0;    
+        : display.value = eval(display.value);
+    resRec = 1;
+    opsBtn = 0;
 }
 
 document.querySelector('.cls').addEventListener('click', clearResults);
@@ -43,4 +44,10 @@ document.querySelector('.dot').addEventListener('click', addDot);
 
 function addDot(ev) {
     if (display.value.slice(-1) !== '.') display.value += ev.target.innerText;
+}
+
+document.querySelector('.delChar').addEventListener('click', delChar);
+
+function delChar() {
+    display.value = display.value.substring(0, display.value.length - 1);
 }
